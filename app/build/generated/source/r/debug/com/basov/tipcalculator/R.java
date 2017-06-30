c4008888d79b9dfcb337334acdf8741e68f93cbc
Yuriy Basov <basowy@gmail.com>
2017-06-29 18:19:56 -0700

    Add styles, themes, and colors

--- app/build/generated/source/r/debug/com/basov/tipcalculator/R.java
+++ app/build/generated/source/r/debug/com/basov/tipcalculator/R.java
@@ -13,56 +13,66 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int colorAccent=0x7f060000;
+        public static final int colorBackground=0x7f060001;
+        public static final int colorPrimary=0x7f060002;
+        public static final int colorPrimaryDark=0x7f060003;
+        public static final int colorWeightInput=0x7f060004;
+    }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int reset=0x7f020002;
     }
     public static final class id {
-        public static final int billAmountEditText=0x7f080001;
-        public static final int billAmountLabel=0x7f080000;
-        public static final int menu_settings=0x7f080014;
-        public static final int noneRadioButton=0x7f08000d;
-        public static final int percentDownButton=0x7f080004;
-        public static final int percentLabel=0x7f080002;
-        public static final int percentTextView=0x7f080003;
-        public static final int percentUpButton=0x7f080005;
-        public static final int persentSeekBar=0x7f080013;
-        public static final int roundRadioGroup=0x7f08000b;
-        public static final int splitAmountTextView=0x7f080012;
-        public static final int splitBillSpinner=0x7f080011;
-        public static final int textView=0x7f08000a;
-        public static final int textView2=0x7f08000c;
-        public static final int textView3=0x7f080010;
-        public static final int tipLabel=0x7f080006;
-        public static final int tipRadioButton=0x7f08000e;
-        public static final int tipTextView=0x7f080007;
-        public static final int totalLabel=0x7f080008;
-        public static final int totalRadioButton=0x7f08000f;
-        public static final int totalTextView=0x7f080009;
+        public static final int billAmountEditText=0x7f090001;
+        public static final int billAmountLabel=0x7f090000;
+        public static final int menu_settings=0x7f090014;
+        public static final int noneRadioButton=0x7f09000d;
+        public static final int percentDownButton=0x7f090004;
+        public static final int percentLabel=0x7f090002;
+        public static final int percentTextView=0x7f090003;
+        public static final int percentUpButton=0x7f090005;
+        public static final int persentSeekBar=0x7f090013;
+        public static final int roundRadioGroup=0x7f09000b;
+        public static final int splitAmountTextView=0x7f090012;
+        public static final int splitBillSpinner=0x7f090011;
+        public static final int textView=0x7f09000a;
+        public static final int textView2=0x7f09000c;
+        public static final int textView3=0x7f090010;
+        public static final int tipLabel=0x7f090006;
+        public static final int tipRadioButton=0x7f09000e;
+        public static final int tipTextView=0x7f090007;
+        public static final int totalLabel=0x7f090008;
+        public static final int totalRadioButton=0x7f09000f;
+        public static final int totalTextView=0x7f090009;
     }
     public static final class layout {
         public static final int activity_tip_calculator=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_tip_calculator=0x7f070000;
+        public static final int activity_tip_calculator=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int bill_amount=0x7f060001;
-        public static final int bill_amount_label=0x7f060002;
-        public static final int decrease=0x7f060003;
-        public static final int increase=0x7f060004;
-        public static final int menu_settings=0x7f060005;
-        public static final int tip_amount=0x7f060006;
-        public static final int tip_amount_label=0x7f060007;
-        public static final int tip_percent=0x7f060008;
-        public static final int tip_percent_label=0x7f060009;
-        public static final int title_activity_tip_calculator=0x7f06000a;
-        public static final int total_amount=0x7f06000b;
-        public static final int total_amount_label=0x7f06000c;
+        public static final int app_name=0x7f070000;
+        public static final int bill_amount=0x7f070001;
+        public static final int bill_amount_label=0x7f070002;
+        public static final int decrease=0x7f070003;
+        public static final int increase=0x7f070004;
+        public static final int menu_settings=0x7f070005;
+        public static final int tip_amount=0x7f070006;
+        public static final int tip_amount_label=0x7f070007;
+        public static final int tip_percent=0x7f070008;
+        public static final int tip_percent_label=0x7f070009;
+        public static final int title_activity_tip_calculator=0x7f07000a;
+        public static final int total_amount=0x7f07000b;
+        public static final int total_amount_label=0x7f07000c;
     }
     public static final class style {
         public static final int AppTheme=0x7f040000;
+        public static final int EditText=0x7f040001;
+        public static final int Label=0x7f040002;
+        public static final int TextView=0x7f040003;
     }
 }
